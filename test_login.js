b4e3a138544a736c12c537487ac5010c9c941f76
Maxilo92 <Priesnitz.maximilian@icloud.com>
2026-03-04 21:23:46 +0100

    Cleanup: Remove obsolete scripts, tests, and temporary data

--- test_login.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = '$2b$10$Bu8f8dNYK.cVgNhO9FY3uu6pPpoN0q.5TD6urMmg8byB.KesMwvC2';
-const pass = 'password123';
-
-bcrypt.compare(pass, hash, (err, res) => {
-    console.log('Result:', res);
-    if (err) console.error('Error:', err);
-});
